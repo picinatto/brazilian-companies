d36dc9c7379cf3d5680664ee1c2081497293ed82
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-05-09 11:31:19 -0300

    create table companies

--- create_tables.py
+++ create_tables.py
@@ -11,47 +11,68 @@ def create_table_companies_south():
   print(f'Started at {datetime.datetime.now()}')
   print('Creating the table companies_south..')
   conDB = sqlite3.connect(db_location+db_name)
-
-  sql = '''CREATE TABLE companies_south 
-            AS SELECT
-              cnpj text,
-              matriz_filial text,
-              razao_social text,
-              nome_fantasia text,
-              cod_nat_juridica text,
-              data_inicio_ativ text,
-              cnae_fiscal text,
-              cep text,
-              uf text,
-              cod_municipio  text,
-              nome_municipio text,
-              qualif_resp text,
-              capital_social text,
-              porte text,
-              opc_simples text,
-              opc_mei text
-
-            FROM empresas WHERE situacao = "02" AND uf IN("PR","SC","RS")'''
-
   cursorDB = conDB.cursor()
 
-  cursorDB.execute(sql)
+  sql_create = '''CREATE TABLE IF NOT EXISTS companies_south (
+      cnpj text,
+      matriz_filial text,
+      razao_social text,
+      nome_fantasia text,
+      cod_nat_juridica text,
+      data_inicio_ativ text,
+      cnae_fiscal text,
+      cep text,
+      uf text,
+      cod_municipio text,
+      municipio text,
+      qualif_resp text,
+      capital_social text,
+      porte text,
+      opc_simples text,
+      opc_mei text
+      );
+   '''
+
+  cursorDB.execute(sql_create)
+
+  sql_insert = '''INSERT INTO companies_south SELECT * FROM (SELECT
+              cnpj,
+              matriz_filial,
+              razao_social,
+              nome_fantasia,
+              cod_nat_juridica,
+              data_inicio_ativ,
+              cnae_fiscal,
+              cep,
+              uf,
+              cod_municipio,
+              municipio,
+              qualif_resp,
+              capital_social,
+              porte,
+              opc_simples,
+              opc_mei
+
+            FROM empresas WHERE situacao = "02" AND uf IN("PR","SC","RS"));'''
+
+  cursorDB.execute(sql_insert)
 
   # Creating indexes
   print('Creating indexes..')
   sql_index = 'CREATE INDEX {} ON {} ({});'.format('ix_cod_municipio', 'companies_south', 'cod_municipio')
-  cursorBD.execute(sql_index)
+  cursorDB.execute(sql_index)
 
-  sql_index = 'CREATE INDEX {} ON {} ({});'.format('ix_nome_municipio', 'companies_south', 'nome_municipio')
-  cursorBD.execute(sql_index)
+  sql_index = 'CREATE INDEX {} ON {} ({});'.format('ix_nome_municipio', 'companies_south', 'municipio')
+  cursorDB.execute(sql_index)
 
   sql_index = 'CREATE INDEX {} ON {} ({});'.format('ix_cnpj', 'companies_south', 'cnpj')
-  cursorBD.execute(sql_index)
+  cursorDB.execute(sql_index)
 
   sql_index = 'CREATE INDEX {} ON {} ({});'.format('ix_porte', 'companies_south', 'porte')
-  cursorBD.execute(sql_index)
+  cursorDB.execute(sql_index)
 
   print(f'Finished at {datetime.datetime.now()}')
+  conDB.close()
 
 def create_table_cities():
   start_time = datetime.datetime.now()
@@ -73,7 +94,7 @@ def create_table_cities():
 
   cursorDB = conDB.cursor()
 
-  cursorDB.execute(sql)
+  cursorDB.execute(sql_create_cities)
 
   print(f'Finished at {datetime.datetime.now()}')
 
